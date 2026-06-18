9cb25d98b5c14b30d0f9f4ab3122cf2684e0bc8b
gukhee <lkh001177@naver.com>
2026-06-18 12:29:18 +0900

    feat : BoardService 기본 구조 추가

--- Board.java
+++ Board.java
@@ -1,6 +1,5 @@
 public class Board {
-private int id;
-private String title;
-private String author;
-// getter/setter 생략
+    private int    id;
+    private String title;
+    private String author;
 }
